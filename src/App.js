ac227d98a8e2d194f91982bfbbabb9e53ea1bb85
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2021-08-08 03:55:27 -0400

    agregar tareas y obtener tareas listo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,79 @@
 import "./App.css";
-import Tarea from "./modules/tareas/page/tareas";
+import React, { useEffect, useState } from "react";
+import { firebase } from "./database/firebase";
 
 function App() {
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState("");
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      const db = firebase.firestore();
+      try {
+        const data = await db.collection("tareas").get();
+        const arrayData = data.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setTareas(arrayData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerDatos();
+  }, []);
+
+  const agregarTarea = async (e) => {
+    e.preventDefault();
+    if (!tarea.trim()) {
+      console.log("esta vacio");
+      return;
+    }
+    try {
+      const db = firebase.firestore();
+      const nuevaTarea = {
+        tarea: tarea,
+      };
+      const data = db.collection("tareas").add({
+        tarea: tarea,
+      });
+      setTareas([...tareas, { id: data.id, ...nuevaTarea }]);
+      setTarea("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="container mt-5">
-      <Tarea />
+    <div className="container mb-2">
+      <div className="row">
+        <div className="col-md-6">
+          <h3>Lista de Tareas</h3>
+          <ul className="list-group">
+            {tareas.map((item) => (
+              <li className="list-group-item" key={item.id}>
+                <span>{item.tarea}</span>
+                <button className="btn btn-warning float-end mx-2">
+                  Editar
+                </button>
+                <button className="btn btn-danger float-end">Eliminar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-md-6">
+          <h3>Formulario</h3>
+          <form onSubmit={agregarTarea}>
+            <input
+              type="text"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+              className="form-control"
+            />
+            <div className="d-grid gap-2">
+              <button className="btn btn-dark mt-3">Agregar</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/modules/login/form/form.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function form() {
-  return (
-    <div>
-      <h1>form</h1>
-    </div>
-  );
-}
